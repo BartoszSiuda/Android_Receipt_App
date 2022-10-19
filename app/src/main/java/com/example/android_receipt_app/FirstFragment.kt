002e9c19a04d8bc87a859f4f1932cc09da391fc4
BartoszSiuda <siudabartosz1@gmail.com>
2022-10-19 02:26:40 +0100

    buttonadded

--- app/src/main/java/com/example/android_receipt_app/FirstFragment.kt
+++ app/src/main/java/com/example/android_receipt_app/FirstFragment.kt
@@ -1,11 +1,11 @@
 package com.example.android_receipt_app
 
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.example.android_receipt_app.databinding.FragmentFirstBinding
 
 /**
@@ -25,12 +25,26 @@ class FirstFragment : Fragment() {
     ): View? {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        setupButtons()
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    private fun setupButtons() {
+
+        binding.button.setOnClickListener {
+
+            fragmentManager?.beginTransaction()?.replace(R.id.nav_host_fragment_content_main,SecondFragment())?.commit()
+        
+
+
+        }
 
 
     }
