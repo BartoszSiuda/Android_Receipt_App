f1485f38513b7c44031e2686e376885e7656c95a
BartoszSiuda <siudabartosz1@gmail.com>
2022-11-25 12:05:00 +0000

    swipe implementation

--- app/src/main/java/com/example/android_receipt_app/FirstFragment.kt
+++ app/src/main/java/com/example/android_receipt_app/FirstFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.android_receipt_app.databinding.FragmentFirstBinding
 import kotlinx.android.synthetic.main.fragment_first.*
 
@@ -59,10 +62,43 @@ class FirstFragment : Fragment() {
         // We send things we wanna display to the adapter.
         fetchReceiptToDisplay(adapter)
 
+        val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
+        itemTouchHelper.attachToRecyclerView(list)
+
     }
 
     private fun fetchReceiptToDisplay(adapter: ListAdapter) {
         val storedReceipts = (activity as MainActivity).receiptsMainStorage
         adapter.setPosts(storedReceipts)
     }
+
+    var simpleItemTouchCallback: ItemTouchHelper.SimpleCallback = object :
+        ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT or ItemTouchHelper.DOWN or ItemTouchHelper.UP
+        ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+
+            Toast.makeText(activity, "on Move", Toast.LENGTH_SHORT).show()
+
+            return false
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, swipeDir: Int) {
+
+            Toast.makeText(activity, "on Swiped ", Toast.LENGTH_SHORT).show()
+
+//            Remove swiped item from list and notify the RecyclerView
+//            val position = viewHolder.adapterPosition
+//            arrayList.remove(position)
+//            adapter.notifyDataSetChanged()
+        }
+    }
+
+
+
 }
