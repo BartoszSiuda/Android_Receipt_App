520b9316aa2e07ae1088e1f14ba95ade5cbad714
BartoszSiuda <siudabartosz1@gmail.com>
2022-11-29 19:53:16 +0000

    validation implemented

--- app/src/main/java/com/example/android_receipt_app/SecondFragment.kt
+++ app/src/main/java/com/example/android_receipt_app/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.android_receipt_app.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
@@ -27,9 +28,20 @@ class SecondFragment : Fragment() {
         }
 
         binding.confirm.setOnClickListener {
+
+            // Todo: Add validation. Check here if the title or description is empty. If any
+            //  of these two is empty - then display the Toast message with text
+            //  "Empty title or description". Otherwise ("else") perform the code below.
             val newReceipt = createReceipt()
-            addReceiptToTheList(newReceipt)
-            openFirstFragment()
+
+            if (binding.Title1.text.isEmpty()) {
+                Toast.makeText(activity, "Empty title", Toast.LENGTH_SHORT).show()
+            } else if (binding.Description1.text.isEmpty()) {
+                Toast.makeText(activity, "Empty description", Toast.LENGTH_SHORT).show()
+            } else {
+                addReceiptToTheList(newReceipt)
+                openFirstFragment()
+            }
         }
     }
 
@@ -37,7 +49,7 @@ class SecondFragment : Fragment() {
         val title = binding.Title1.text.toString()
         val image = "https://upload.wikimedia.org/wikipedia/commons/b/bb/Carmen_Electra_2013.jpg"
         val description = binding.Description1.text.toString()
-      
+
         return ReceiptEntity(
             title,
             image,
