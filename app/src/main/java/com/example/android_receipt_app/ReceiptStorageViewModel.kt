072a05ba8606546b87d297bb3266d16ce68f8a55
BartoszSiuda <siudabartosz1@gmail.com>
2022-11-30 13:06:09 +0000

    removal fun partially

--- app/src/main/java/com/example/android_receipt_app/ReceiptStorageViewModel.kt
+++ app/src/main/java/com/example/android_receipt_app/ReceiptStorageViewModel.kt
@@ -7,20 +7,9 @@ class ReceiptStorageViewModel {
 
      fun removeReceiptFromMainStorage(receiptToDelete: ReceiptEntity) {
 
-
-
+         receiptsMainStorage.remove(receiptToDelete)
 
     }
 
-
-    // Todo: We will implement proper removing of the receipt from our list.
-    // First step to do this is: create here (in the ViewModel) a new function
-    // "removeReceiptFromMainStorage()" (empty function for now).
-
-    // Second step: make this function receiving a variable (box) named "receiptToDelete", and being
-    // a type of "ReceiptEntity". It will be indicating the receipt we wanna remove from the list.
-
-    // Third step: implement the body of this new function. In this function we just need a one line.
-    // It will be "receiptsMainStorage.remove(RECEIPT_YOU_WANT_TO_REMOVE_VARIABLE)".
-    // This line removes given element from the list.
+    
 }
