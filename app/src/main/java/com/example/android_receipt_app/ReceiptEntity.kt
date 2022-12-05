52b4fccd4a1bd3dcb7b02a65452947ac3a36fe34
BartoszSiuda <siudabartosz1@gmail.com>
2022-12-05 23:53:49 +0000

    new button implemented

--- app/src/main/java/com/example/android_receipt_app/ReceiptEntity.kt
+++ app/src/main/java/com/example/android_receipt_app/ReceiptEntity.kt
@@ -3,6 +3,7 @@ package com.example.android_receipt_app
 data class ReceiptEntity(
         val title: String?,
         val image: String?,
-        val description: String?
+        val description: String?,
+        val creationDate: String?
 )
 
