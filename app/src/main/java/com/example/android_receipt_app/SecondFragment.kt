55f2bd36222c9a63f0536e8318700cfd8ed67294
BartoszSiuda <siudabartosz1@gmail.com>
2022-12-06 00:18:18 +0000

    new button implemented

--- app/src/main/java/com/example/android_receipt_app/SecondFragment.kt
+++ app/src/main/java/com/example/android_receipt_app/SecondFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.example.android_receipt_app.databinding.FragmentSecondBinding
+import kotlinx.android.synthetic.main.single_receipt.*
 
 class SecondFragment : Fragment() {
 
@@ -46,11 +47,14 @@ class SecondFragment : Fragment() {
         val title = binding.Title1.text.toString()
         val image = "https://upload.wikimedia.org/wikipedia/commons/b/bb/Carmen_Electra_2013.jpg"
         val description = binding.Description1.text.toString()
+        val creationDate = binding.Description1.text.toString()
 
         return ReceiptEntity(
             title,
             image,
-            description
+            description,
+            creationDate,
+
         )
     }
 
